6eb63047730b263af2c481b624b62cf63aaedf0b
zenk <zengkai97@outlook.com>
2022-10-30 10:07:42 +0800

    new rust project

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,24 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // tauri 配置
+  // 防止 vite 输出复杂的 rust 错误
+  clearScreen: false,
+  // Tauri 使用固定端口，若此端口不可用将会导致程序错误
+  server: {
+    strictPort: true,
+  },
+  // 使用 `TAURI_PLATFORM`、`TAURI_ARCH`、`TAURI_FAMILY`,
+  // `TAURI_PLATFORM_VERSION`、`TAURI_PLATFORM_TYPE` 和 `TAURI_DEBUG` 环境变量
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri 支持 es2021
+    target: ['es2021', 'chrome100', 'safari13'],
+    // 不为调试构建压缩构建体积
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // 为调试构建生成源代码映射 (sourcemap)
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+
   plugins: [vue()]
 })
